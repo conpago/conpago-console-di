5a0a8a6d4dabbb4aa8b9c2aead6ff1f7a93f4cbc
Bartosz Gołek <bartosz.golek@gmail.com>
2015-06-17 09:31:47 +0200

    Fixed psr-0 in composer.json file

--- src/Conpago/Console/ConsoleModule.php
+++ src/Conpago/Console/ConsoleModule.php
@@ -6,29 +6,29 @@
 	 * Time: 23:45
 	 */
 
-	namespace Saigon\Conpago\Console;
+	namespace Conpago\Console;
 
 
-	use Saigon\Conpago\DI\IContainerBuilder;
-	use Saigon\Conpago\DI\IModule;
+	use Conpago\DI\IContainerBuilder;
+	use Conpago\DI\IModule;
 
 	class ConsoleModule implements IModule {
 
 		public function build( IContainerBuilder $builder ) {
 			$builder
-				->registerType('Saigon\Conpago\Console\Presentation\ConsolePresenter')
-				->asA('Saigon\Conpago\Console\Contract\Presentation\IConsolePresenter');
+				->registerType('Conpago\Console\Presentation\ConsolePresenter')
+				->asA('Conpago\Console\Contract\Presentation\IConsolePresenter');
 
 			$builder
-				->registerType('Saigon\Conpago\Console\HelpCommand')
-				->asA('Saigon\Conpago\Console\Contract\ICommand');
+				->registerType('Conpago\Console\HelpCommand')
+				->asA('Conpago\Console\Contract\ICommand');
 
 			$builder
-				->registerType('Saigon\Conpago\Console\Presentation\HelpCommandPresenter')
-				->asA('Saigon\Conpago\Console\Contract\Presentation\IHelpCommandPresenter');
+				->registerType('Conpago\Console\Presentation\HelpCommandPresenter')
+				->asA('Conpago\Console\Contract\Presentation\IHelpCommandPresenter');
 
 			$builder
-				->registerType('Saigon\Conpago\Console\ConsoleApp')
-				->asA('Saigon\Conpago\Contract\IApp');
+				->registerType('Conpago\Console\ConsoleApp')
+				->asA('Conpago\Contract\IApp');
 		}
 	}
